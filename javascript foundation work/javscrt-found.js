54a07bfa30d7cabd52bf12589b60efce1511293f
Hayden-Neilson <haydenneilson5@gmail.com>
2020-12-18 07:57:55 -0700

    loops for js

---
+++ javascript foundation work/javscrt-found.js
@@ -0,0 +1,68 @@
+var name, city, age;
+name = 'Kristine';
+city = 'Scottsdale';
+age = 12;
+
+console.log(age);
+console.log(name);
+console.log(city);
+
+
+
+
+
+
+
+
+// How to Use the Splice Function in JavaScript to Remove Specific Array Elements
+
+var arr = ['Altuve', 'Bregman', 'Correa', 'Springer'];
+
+var foundElement = arr.indexOf('Correa');
+
+foundElement; // 2
+
+arr.splice(foundElement, 1); // ["Correa"]
+
+arr; // ["Altuve", "Bregman", "Springer"]
+
+arr.splice(1, 2); // ["Bregman", "Springer"]
+
+arr; // ["Altuve"]
+
+
+
+// Guide to For Loops in JavaScript
+
+
+var players = [
+    'Altuve',
+    'Bregman',
+    'Correa',
+    'Springer'
+  ];
+//   For
+  for (player in players) {
+    console.log(players[player]);
+  }
+//   for in
+  for (var i = 0; i < players.length; i++) {
+    console.log(players[i]);
+  }
+//   forEach
+  players.forEach(function(element) {
+    console.log(element);
+  });
+
+//   How to Loop Through a JavaScript Object
+
+  var student = {
+    name: 'Kristine',
+    age: 12,
+    city: 'Scottsdale'
+  };
+  
+  for (var key in student) {
+    console.log(key + " => " + student[key]);
+  }
+  
