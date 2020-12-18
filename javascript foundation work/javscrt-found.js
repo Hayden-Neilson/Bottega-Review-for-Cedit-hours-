5f9c0ce3b8aaef07e42d8f5af3a6e0d167986b42
Hayden-Neilson <haydenneilson5@gmail.com>
2020-12-18 08:03:28 -0700

    do/while and while loops

--- javascript foundation work/javscrt-found.js
+++ javascript foundation work/javscrt-found.js
@@ -66,3 +66,23 @@ var players = [
     console.log(key + " => " + student[key]);
   }
   
+
+
+  var players = [
+    'Altuve',
+    'Bregman',
+    'Correa',
+    'Springer'
+  ];
+//   while
+  var i = 0;
+  while (i < players.length) {
+    console.log(players[i]);
+    i++;
+  }
+//   do/while
+  var i = 21;
+  do {
+    console.log(players[i]);
+    i++;
+  } while (i < players.length)
