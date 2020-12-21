540185637b15901ca7e03fd2e16b466eb9e7b805
Hayden-Neilson <haydenneilson5@gmail.com>
2020-12-20 18:52:57 -0700

    contact form and styles

--- javascript foundation work/javscrt-found.js
+++ javascript foundation work/javscrt-found.js
@@ -105,3 +105,14 @@ images.forEach(img => console.log(img.src));
 let imageUrlArray = [];
 
 images.forEach(img => imageUrlArray.push(img.src));
+
+
+// Auto Following Accounts and Hashtags on LinkedIn with JavaScript
+
+let hashtagBtns = document.querySelectorAll('.mn-discovery-hashtag-card__action-btn')
+
+hashtagBtns.forEach(btn => btn.click())
+
+const followingBtns = document.querySelectorAll('.follows-recommendation-card__follow-btn')
+
+followingBtns.forEach(followbtn => followbtn.click())
