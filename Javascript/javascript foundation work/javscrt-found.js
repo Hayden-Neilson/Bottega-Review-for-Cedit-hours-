23bdcd30570979ed15dbfdae7ca1f1a49732a085
Hayden-Neilson <haydenneilson5@gmail.com>
2021-01-03 19:22:51 -0700

    hoisting

--- Javascript/javascript foundation work/javscrt-found.js
+++ Javascript/javascript foundation work/javscrt-found.js
@@ -1,11 +1,15 @@
+// variable work
 var name, city, age;
-name = 'Kristine';
+var name = 'Kristine';
 city = 'Scottsdale';
 age = 12;
 
 console.log(age);
 console.log(name);
 console.log(city);
+
+//Hoisting means your var must be declared 
+// before trying to return it otherwise youll get undefined
 
 
 
