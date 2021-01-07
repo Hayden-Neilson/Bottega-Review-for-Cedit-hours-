affde531896e9ba255c9651fcc01f4a607c54f2c
Hayden-Neilson <haydenneilson5@gmail.com>
2021-01-06 19:56:48 -0700

    upper lower cap

---
+++ Python/variable.py
@@ -0,0 +1,17 @@
+meal_completed = True
+sub_total = 100
+tip = sub_total * 1/5
+total = sub_total + tip
+receipt = "Your total is " + str(total)
+print(receipt)
+
+# types of data types
+Booleans
+Numbers
+Strings
+Bytes and byte arrays
+None
+Lists
+Tuples
+Sets
+Dictionaries
