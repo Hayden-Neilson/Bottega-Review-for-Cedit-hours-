9023ba2d46acedc4c9db66352b53364b34791d18
Hayden-Neilson <haydenneilson5@gmail.com>
2021-01-24 16:10:07 -0700

    objects and datatypes

---
+++ Javascript/js objectpract.js
@@ -0,0 +1,11 @@
+var user = {
+    name: 'Kristine',
+    age: 12,
+    city: 'Scottsdale',
+    grades: {
+      math: 90,
+      science: 80,
+      languageArts: 100
+    }
+  }
+  user.grades.english =87
