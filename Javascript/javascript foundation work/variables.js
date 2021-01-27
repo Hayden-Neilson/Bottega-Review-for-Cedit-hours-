2d500e231b305deb0478c3eb6e7e4dff99985417
Hayden-Neilson <haydenneilson5@gmail.com>
2021-01-27 05:06:42 -0700

    more vars

--- Javascript/javascript foundation work/variables.js
+++ Javascript/javascript foundation work/variables.js
@@ -8,6 +8,16 @@ console.log(age);
 console.log(name);
 console.log(city);
 
+
+var name, city, age;
+name = 'Kristine';
+city = 'Scottsdale';
+age = 12;
+
+console.log(age);
+console.log(name);
+console.log(city);
+
 //Hoisting means your var must be declared 
 // before trying to return it otherwise youll get undefined
 
