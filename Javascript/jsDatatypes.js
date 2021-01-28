791bc164775c0cbc2babed45a812165b9c4d473f
Hayden-Neilson <haydenneilson5@gmail.com>
2021-01-28 06:09:25 -0700

    datatypes

--- Javascript/jsDatatypes.js
+++ Javascript/jsDatatypes.js
@@ -19,3 +19,25 @@ var nameTwo = 'Jordan';
 var mySym = Symbol('foo');
 
 console.log(mySym);
+
+// Boolean
+var truthy = true;
+var notTruthy = false;
+
+// Null
+var nully = null;
+
+// Undefined
+var notDefined;
+
+// Number
+var age = 12;
+
+// String
+var name = "Kristine";
+var nameTwo = 'Jordan';
+
+// Symbol
+var mySym = Symbol('foo');
+
+console.log(mySym);
